477d1bd107a97fce89e707c7bfeaaa1c45e1b46f
dianasilvaaaa <dianabarross9@gmail.com>
2024-09-23 16:03:14 +0100

    add csv_file e data_file

---
+++ src/si/io/csv_file.py
@@ -0,0 +1,9 @@
+def read_csv(filename: str, sep: str = "", features, label):
+
+"""   
+filename – name/path of the file
+sep – value separator
+features – boolean. Does the file have feature names?
+label – boolean. Does the file have y? (If yes, assume it's the last column)
+
+"""   
