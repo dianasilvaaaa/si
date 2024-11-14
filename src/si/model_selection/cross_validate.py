49e797351107e2d14040164299f09e643fea22ff
dianasilvaaaa <dianabarross9@gmail.com>
2024-11-14 11:07:57 +0000

    up

--- src/si/model_selection/cross_validate.py
+++ src/si/model_selection/cross_validate.py
@@ -5,4 +5,4 @@ from si.base.model import Model
 from si.data.dataset import Dataset
 
 
-def K_fold_cross_validation(model: Model, dataset: Dataset, scoring: callable, cv:int, )
+#def K_fold_cross_validation(model: Model, dataset: Dataset, scoring: callable, cv:int, )
